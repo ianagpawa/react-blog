c0bf425e025b942078663904ad658521736a2aa2
Ian Agpawa <agpawaji@gmail.com>
2018-03-24 23:08:18 -0400

    feat: Add fetch post reducer and action

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 export const FETCH_POSTS = 'fetch_posts';
 export const CREATE_POST = 'create_post';
+expoert const FETCH_POST = 'fetch_post';
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=PI314159265432'
@@ -24,4 +25,13 @@ export function createPost(values, callback){
         type: CREATE_POST,
         payload: request
     };
+}
+
+export function fetchPost(id){
+    const request = axios.get(`${ROOT_URL}/posts/${id}`);
+
+    return {
+        type: FETCH_POST,
+        payload: request
+    }
 }
