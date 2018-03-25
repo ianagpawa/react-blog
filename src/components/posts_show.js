18c5cddb7094cba0bbf4c3325dd83ffbdbbdb1e7
Ian Agpawa <agpawaji@gmail.com>
2018-03-25 16:58:54 -0400

    Add html for rendering post, add default for load rendering

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -12,9 +12,16 @@ class PostsShow extends Component {
 
     render(){
 
+        const { post }= this.props;
+
+        if (!post){
+            return <div>Loading...</div>;
+        }
         return (
             <div>
-                Posts Show!
+                <h3>{post.title}</h3>
+                <h6>Categories: {post.categories}</h6>
+                <p>{post.content}</p>
             </div>
         );
     }
@@ -22,7 +29,7 @@ class PostsShow extends Component {
 
 
 function mapStateToProps({ posts }, ownProps){
-    return { post: posts[ownProprs.match.params.id] };
+    return { post: posts[ownProps.match.params.id] };
 }
 
 export default connect(mapStateToProps, { fetchPost })(PostsShow);
