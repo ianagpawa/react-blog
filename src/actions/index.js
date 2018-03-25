0ce80fda6943dc7bfdcf5fc2a308d36938378804
Ian Agpawa <agpawaji@gmail.com>
2018-03-25 01:16:11 -0400

    feat: Correct export for fetch

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const FETCH_POSTS = 'fetch_posts';
 export const CREATE_POST = 'create_post';
-expoert const FETCH_POST = 'fetch_post';
+export const FETCH_POST = 'fetch_post';
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=PI314159265432'
